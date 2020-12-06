7513a8c85f950a8e7ee8e3679fb2aee905fc53c7
dstevens <david.stevens@res-group.com>
2020-12-06 12:09:25 +0100

    Fix adafruit sensor build dependency after platformio update

--- projects/iot/src/main.cpp
+++ projects/iot/src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-
-#include <DHT.h>
 
 #include "secrets.h"
 const char *ssid     = SECRET_SSID;
